3a7b33c766fc7b91c4d45a8e25963ac898931a72
BenNeighbour <benneighbour007@gmail.com>
2019-08-04 10:50:21 +0100

    Bug Fixes

--- src/main/java/com/benneighbour/loginapp/loginView.java
+++ src/main/java/com/benneighbour/loginapp/loginView.java
@@ -4,7 +4,10 @@ package com.benneighbour.loginapp;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -24,6 +27,11 @@ public class loginView extends VerticalLayout {
 
     // Creating the main view of the application
     public loginView() {
+
+        Tab mr = new Tab("Male");
+        Tab mrs = new Tab("Female");
+        Tab rathernotsay = new Tab("Other");
+        Tabs tabs = new Tabs(mr, mrs, rathernotsay);
 
         TextField forename = new TextField();
         forename.setLabel("Forename");
@@ -58,6 +66,10 @@ public class loginView extends VerticalLayout {
         });
 
 
+        HorizontalLayout top = new HorizontalLayout(
+            tabs
+        );
+
         VerticalLayout app = new VerticalLayout(
             forename,
             surname,
@@ -69,6 +81,7 @@ public class loginView extends VerticalLayout {
 
 
         add(
+            top,
             app
         );
 
